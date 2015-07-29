b862c18a77c0c430516abb23ac0dd1eea7fc94bd
covercash2 <covercash2@gmail.com>
2015-07-29 11:06:03 -0500

    updated feed cells

--- src/main/groovy/com/github/covercash2/gdreddit/Data.groovy
+++ src/main/groovy/com/github/covercash2/gdreddit/Data.groovy
@@ -4,29 +4,80 @@ import griffon.transform.FXObservable
 import javafx.fxml.FXML
 import javafx.fxml.FXMLLoader
 import javafx.scene.Node
+import javafx.scene.Parent
 import javafx.scene.control.Label
+import javafx.scene.control.TitledPane
+import javafx.scene.image.ImageView
 import javafx.scene.layout.HBox
+import javafx.scene.layout.Pane
+import javafx.scene.layout.VBox
+import javafx.scene.web.WebView
 
 /**
  * Created by covercash on 7/26/15.
  */
 class Data {
-    Label lTitle
-    Label lAuthor
-    Label lScore
+    ImageView ivThumb = new ImageView()
+
+    Label lTitle = new Label()
+    Label lAuthor = new Label()
+    Label lScore = new Label()
+    Label lComments = new Label()
+    Label lSelfText = new Label()
+    Label lSubreddit = new Label()
+
+    TitledPane tpContent = new TitledPane()
+
+    Pane pContent = new Pane()
 
     Node box
 
     Data() {
-        lTitle = new Label()
-        lAuthor = new Label()
-        lScore = new Label()
+        tpContent.with {
+            text = 'test'
+            content = pContent
+            expanded = false
+        }
 
-        box = new HBox(spacing: 10)
-        box.children.addAll([
-                lScore,
-                lTitle,
-                lAuthor
+        HBox detailBox = new HBox(spacing: 10)
+        detailBox.children.addAll([
+                lSubreddit,
+                lComments,
+                lAuthor,
         ])
+
+        VBox titleBox = new VBox(spacing: 10)
+        titleBox.children.addAll([
+                lTitle,
+                detailBox,
+                tpContent
+        ])
+
+        HBox mainBox = new HBox(spacing: 10)
+        mainBox.children.addAll([
+                lScore,
+                ivThumb,
+                titleBox
+        ])
+
+        box = mainBox
+    }
+
+    void addContent(String content) {
+        pContent.children.add(new Label(content))
+    }
+
+    void addHtml(Pane root, String url, boolean javascript) {
+        WebView web = new WebView()
+        web.getEngine().load(url)
+        web.engine.setJavaScriptEnabled(javascript)
+        root.children.add(web)
+    }
+
+    void addHtmlContent(Pane root, String content, boolean javascript) {
+        WebView web = new WebView()
+        web.getEngine().loadContent(content)
+        web.engine.setJavaScriptEnabled(javascript)
+        root.children.add(web)
     }
 }
--- src/main/groovy/com/github/covercash2/gdreddit/FeedCell.groovy
+++ src/main/groovy/com/github/covercash2/gdreddit/FeedCell.groovy
@@ -1,6 +1,8 @@
 package com.github.covercash2.gdreddit
 
 import javafx.scene.control.ListCell
+import javafx.scene.image.Image
+import org.apache.commons.lang3.StringEscapeUtils
 import org.spacehq.reddit.data.json.Link
 
 /**
@@ -15,8 +17,33 @@ class FeedCell extends ListCell<Link> {
 
             data.with {
                 lTitle.text = link.title
-                lAuthor.text = link.author
+                lAuthor.text = '/u/' + link.author
                 lScore.text = link.ups
+                lComments.text = link.num_comments + ' comments'
+                lSelfText.text = link.selftext
+                lSubreddit.text = '/r/' + link.subreddit
+            }
+
+            println link.media
+            println link.media_embed
+            println link.secure_media_embed
+
+            println link.thumbnail
+
+            if (link.thumbnail) {
+                try {
+                    Image thumb = new Image(link.thumbnail, true)
+                    data.ivThumb.image = thumb
+                } catch (IllegalArgumentException e) {
+                    println 'thumbnail not found: ' + link.thumbnail
+                }
+            }
+
+            if (link.selftext) {
+                data.addContent(link.selftext)
+            } else if (link.secure_media_embed) {
+                def embed = StringEscapeUtils.unescapeHtml4(link.secure_media_embed.get('content'))
+                data.addHtmlContent(data.pContent, embed, true)
             }
 
             setGraphic(data.box)
